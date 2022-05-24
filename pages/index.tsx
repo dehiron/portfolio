da3c2a05c50295fc9679faf2586eac558f24b4c2
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-05-24 13:47:37 +0900

    Implemented the projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,12 @@ import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon, BadgeCheckIcon } from '@heroicons/react/solid'
 import { ArrowCircleDownIcon } from '@heroicons/react/outline'
 import { Link as Scroll } from "react-scroll";
+import 'swiper/css/bundle'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Pagination, Navigation } from 'swiper'
+import { getWindowSize } from '../lib/getWindowSize';
+
+SwiperCore.use([Pagination, Navigation]) 
 
 const devFields = [
   { field: 'フロントエンド' },
@@ -21,8 +27,17 @@ const fieldContents = {
   infra: ["GCP", "Firebaseサービス全般", "Vercel Hosting", "Github", "Slack"]
 }
 
+const images = [
+  {
+    title: "TEXTTEXTTEXTTEXTTEXTTEXTTEXT",
+    text: "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext",
+    imgPath: "/fitto_intro.png",
+  },
+]
+
 const index = () => {
 
+  const {width, height} = getWindowSize();
   const [selectedDevField, setSelectedDevField] = useState(devFields[0])
 
   return (
@@ -92,7 +107,7 @@ const index = () => {
               UIUX・デザイン・データ構造等のプロダクト設計からコードに落とし込む実装開発まで一貫して行えることを強みとしており、0 → 1 でアイディアを形にすることを得意としています。
             </p>
             <p className="text-sm md:text-lg whitespace-pre-wrap font-josefin  text-[#323232]">
-              趣味は映画、キャンプ、バイク、旅、サウナ、美味しいものを食べることと美味しいお酒を飲むこととです。仕事中は引きこもりがちなので、休みの時はアクティブに動き回ります笑
+              趣味は映画を観ること、キャンプ、バイク、旅、サウナです。仕事中は引きこもりがちなので、休みの時はアクティブに動き回ります。
             </p>
             <p className="text-sm md:text-lg whitespace-pre-wrap font-josefin  text-[#323232]">
               プロジェクトのご提案や業務提携・その他ご質問やご興味等あれば是非お気軽にコンタクトフォームからご連絡下さい。
@@ -112,7 +127,7 @@ const index = () => {
         <div className="h-1/5 flex items-center justify-center">
           <h3 className=" text-2xl md:text-4xl font-josefin my-10 md:my-12 border-b-4">Skills</h3>
         </div>
-        <div className="h-4/5 md:flex items-center md:space-x-4 w-full px-5 pt-12 pb-14 mb-5 rounded-xl md:shadow-xl md:border">
+        <div className="h-4/5 md:flex items-center md:space-x-4 w-full px-5 pt-12 pb-14 mb-5 rounded-xl shadow-sm md:border md:border-gray-200">
           <div className="sm: w-full md:w-1/3 flex-col items-center justify-center mt-3 md:mt-0">
             <div className="flex items-center justify-center mt-3">
               <img 
@@ -258,25 +273,25 @@ const index = () => {
             <div className=" flex flex-col items-start justify-center px-5 py-3 space-y-3 text-gray-600">
               { selectedDevField.field === "フロントエンド" ? 
                 fieldContents.frontEnd.map((content, contentIndex) => (
-                  <div className="flex items-center justidy-center">
+                  <div className="flex items-center justidy-center" key={contentIndex}>
                     <BadgeCheckIcon className="h-4 w-4 text-gray-500 mr-1"/>
-                    <p className="text-sm text-start" key={contentIndex}>{content}</p>
+                    <p className="text-sm text-start">{content}</p>
                   </div>
                 ))
                 :
                 selectedDevField.field === "バックエンド" ? 
                 fieldContents.backEnd.map((content, contentIndex) => (
-                  <div className="flex items-center justidy-center">
+                  <div className="flex items-center justidy-center" key={contentIndex}>
                     <BadgeCheckIcon className="h-4 w-4 text-gray-500 mr-1"/>
-                    <p className="text-sm text-start" key={contentIndex}>{content}</p>
+                    <p className="text-sm text-start">{content}</p>
                   </div>
                 ))
                 :
                 selectedDevField.field === "インフラ／DevOps" ? 
                 fieldContents.infra.map((content, contentIndex) => (
-                  <div className="flex items-center justidy-center">
+                  <div className="flex items-center justidy-center" key={contentIndex}>
                     <BadgeCheckIcon className="h-4 w-4 text-gray-500 mr-1"/>
-                    <p className="text-sm text-start" key={contentIndex}>{content}</p>
+                    <p className="text-sm text-start">{content}</p>
                   </div>
                 ))
                 :
@@ -312,23 +327,23 @@ const index = () => {
               </h3>
             </div>
             <div className="flex flex-col items-start justify-center px-5 py-3 space-y-3 text-gray-600">
-              <div className="flex items-center justidy-center">
+              <div className="flex items-center justify-center">
                 <BadgeCheckIcon className="h-4 w-4 text-gray-500 mr-1"/>
                 <p className="text-sm text-start">0 → 1フェーズのプロダクト・新規事業開発</p>
               </div>
-              <div className="flex items-center justidy-center">
+              <div className="flex items-center justify-center">
                 <BadgeCheckIcon className="h-4 w-4 text-gray-500 mr-1"/>
                 <p className="text-sm text-start">顧客ニーズ開拓の為のユーザーインタビュー</p>
               </div>
-              <div className="flex items-center justidy-center">
+              <div className="flex items-center justify-center">
                 <BadgeCheckIcon className="h-4 w-4 text-gray-500 mr-1"/>
                 <p className="text-sm text-start">Pd機能改善の為のユーザーヒアリングとテスト</p>
               </div>
-              <div className="flex items-center justidy-center">
+              <div className="flex items-center justify-center">
                 <BadgeCheckIcon className="h-4 w-4 text-gray-500 mr-1"/>
                 <p className="text-sm text-start">某企業での社内DX推進ツール導入</p>
               </div>
-              <div className="flex items-center justidy-center">
+              <div className="flex items-center justify-center">
                 <BadgeCheckIcon className="h-4 w-4 text-gray-500 mr-1"/>
                 <p className="text-sm text-start">トレンドに基づいたIT/DXソリューション提案</p>
               </div>
@@ -343,9 +358,190 @@ const index = () => {
 
       {/* *************************************Projectsセクション***************************************************************************************** */}
 
-      <div className="flex flex-col min-h-[100vh] max-w-7xl mx-auto">
-        <div className="flex items-center justify-center">
-          <h3 className=" text-2xl md:text-4xl font-josefin my-20 border-b-4">Projects</h3>
+      <div className="flex flex-col min-h-[100vh] h-screen max-w-7xl mx-auto">
+        <div className="h-1/5 flex items-center justify-center">
+          <h3 className=" text-2xl md:text-4xl font-josefin my-10 md:my-12 border-b-4">Projects</h3>
+        </div>
+        <div className="h-4/5 md:flex items-center md:space-x-4 w-full px-5 pb-6 mb-3">
+          <Swiper
+            slidesPerView={1} //一度に表示するスライドの数
+            pagination={{ clickable: true }} 
+            // navigation 
+            loop={false}
+            className="h-full"
+          >
+            <SwiperSlide className="flex items-start justify-center h-full" >
+              <div className="w-1/2 flex flex-col items-start justify-center h-full space-y-3 md:space-y-6">
+
+                <div className="flex items-center justify-center px-3 md:px-12">
+                  <p className="text-2xl md:text-4xl whitespace-pre-wrap font-josefin text-gray-600">
+                    fitto
+                  </p>
+                </div>
+
+                {width < 640 ? 
+                <div className="flex flex-col items-start justify-center px-3 md:px-12 space-y-1">
+                  <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                    忙しい・疲れた・やる気が出ない。。
+                  </p>
+                  <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                    そんなサボりマインドでジムがなかなか続かない人の為に、新感覚フィットネスアプリ「fitto」を開発しました。
+                  </p>
+                </div>
+                :
+                <div className="flex flex-col items-start justify-center px-3 md:px-12 space-y-1">
+                  <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                    忙しい・疲れた・やる気が出ない。。
+                  </p>
+                  <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                    そんなサボりマインドでジムがなかなか続かない人の為に、
+                  </p>
+                  <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                    新感覚フィットネスアプリ「fitto」を開発しました。
+                  </p>
+                </div>
+                }
+
+                <div className="flex flex-col w-full items-start justify-center px-3 md:px-12 space-y-2">
+                  <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600 border-b-2">
+                    使用テックスタック
+                  </p>
+                  <div className="w-full flex flex-col md:flex-row">
+                    <div className="md:w-1/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        FE
+                      </p>
+                    </div>
+                    <div className="md:w-5/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        React Native, Expo, JavaScript, TailwindCSS
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full flex flex-col md:flex-row">
+                    <div className="md:w-1/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        BE&Infra
+                      </p>
+                    </div>
+                    <div className="md:w-5/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        NodeJS, Firebase Services, Cloud Functions by GCP
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full flex flex-col md:flex-row">
+                    <div className="md:w-1/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        Others
+                      </p>
+                    </div>
+                    <div className="md:w-5/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        Stripe API(決済)、Google Map API（位置情報）
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col items-start justify-center px-3 md:px-12 pt-3">
+                  <img
+                    className="h-8 md:h-12" 
+                    src={"/app_store_logo.svg"}
+                  />
+                </div>
+              </div>
+              <div className="w-1/2 flex items-center justify-center h-full">
+                <img
+                  className="h-full object-contain"
+                  src={  width < 640 ? "/fitto_intro_responsive.png" : "/fitto_intro_website.png"}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="flex items-start justify-center h-full" >
+              <div className="w-1/2 flex flex-col items-start justify-center h-full space-y-3 md:space-y-10">
+
+                <div className="flex items-center justify-center px-3 md:px-12">
+                  <p className="text-2xl md:text-4xl whitespace-pre-wrap font-josefin text-gray-600">
+                    Haya Lab Website
+                  </p>
+                </div>
+
+
+                {width < 640 ? 
+                  <div className="flex flex-col items-start justify-center px-3 md:px-12 space-y-1">
+                    <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                      今ご覧いただいている本ウェブサイトも、デザイン・要素の構成からスクラッチベースでの開発までワンストップで作成しました。
+                    </p>
+                  </div>
+                  :
+                  <div className="flex flex-col items-start justify-center px-3 md:px-12 space-y-1">
+                    <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                      今ご覧いただいている本ウェブサイトも、
+                    </p>
+                    <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                      デザイン・要素の構成からスクラッチベースでの開発まで
+                    </p>
+                    <p className="text-xs md:text-lg whitespace-pre-wrap font-josefin text-gray-600">
+                      ワンストップで作成しました。
+                    </p>
+                  </div>
+                }
+
+                <div className="flex flex-col w-full items-start justify-center px-3 md:px-12 space-y-2">
+                <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600 border-b-2">
+                    使用テックスタック
+                  </p>
+                  <div className="w-full md:flex md:flex-col">
+                    <div className="md:w-1/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        Design
+                      </p>
+                    </div>
+                    <div className="md:w-5/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        AdobeXd, Illustrator
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full md:flex md:flex-col">
+                    <div className="md:w-1/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        FE
+                      </p>
+                    </div>
+                    <div className="md:w-5/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        NextJs, TypeScript, HeadlessUI, TailwindCSS
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full md:flex md:flex-col">
+                    <div className="md:w-1/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        BE&Infra
+                      </p>
+                    </div>
+                    <div className="md:w-5/6">
+                      <p className="text-[8px] md:text-sm whitespace-pre-wrap font-josefin text-gray-600">
+                        NodeJS, Nodemailer, Vercel Hosting
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+
+
+
+              </div>
+              <div className="w-1/2 flex items-center justify-center h-full">
+                <img
+                  className="h-full object-contain"
+                  src={  width < 640 ? "/haya_lab_responsive.png" : "/haya_lab_website.png"}
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
 
